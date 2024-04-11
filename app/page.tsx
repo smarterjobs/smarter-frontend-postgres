07d53eb7b958f8a05b4def92898eaa0bff8b2f21
jamoowen <jamow.owen@gmail.com>
2024-04-11 20:39:25 +0100

    test commit

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ export default async function IndexPage({
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
       <Title>Users</Title>
-      <Text>A list of users retrieved from a Postgres database.</Text>
+      <Text>A Yehllo.</Text>
       <Search />
       <Card className="mt-6">
         <UsersTable users={users} />
